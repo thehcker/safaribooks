257cb9a494616e03939c63959309101aa6155ab6
thehcker <isaacnyakoi1@gmail.com>
2018-10-15 02:00:46 +0300

    Heroku deployment changes

--- app/__init__.py
+++ app/__init__.py
@@ -68,17 +68,17 @@ def create_app(config_class=Config):
             app.logger.addHandler(stream_handler)
         else:
 
-        if not os.path.exists('logs'):
-            os.mkdir('logs')
-        file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240,
-                                           backupCount=10)
-        file_handler.setFormatter(logging.Formatter(
-            '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
-        file_handler.setLevel(logging.INFO)
-        app.logger.addHandler(file_handler)
+            if not os.path.exists('logs'):
+                os.mkdir('logs')
+            file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240,
+                                               backupCount=10)
+            file_handler.setFormatter(logging.Formatter(
+                '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+            file_handler.setLevel(logging.INFO)
+            app.logger.addHandler(file_handler)
 
-        app.logger.setLevel(logging.INFO)
-        app.logger.info('Microblog startup')
+            app.logger.setLevel(logging.INFO)
+            app.logger.info('Microblog startup')
 
     return app
 
