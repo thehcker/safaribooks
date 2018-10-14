e2f3685f48dc133d0d2112a05e4eb5dd628e2ade
thehcker <isaacnyakoi1@gmail.com>
2018-10-14 23:57:50 +0300

    Add requirements.txt file

--- app/main/routes.py
+++ app/main/routes.py
@@ -10,7 +10,7 @@ from datetime import datetime
 from flask_babel import _, get_locale
 from guess_language import guess_language
 from app.translate import translate
-from app.main.forms import EditProfileForm, PostForm
+from app.main.forms import EditProfileForm, PostForm,SearchForm
 
 
 @bp.before_app_request
@@ -18,6 +18,7 @@ def before_request():
     if current_user.is_authenticated:
         current_user.last_seen = datetime.utcnow()
         db.session.commit()
+        g.search_form = SearchForm()
     g.locale = str(get_locale())
 
 
@@ -133,3 +134,18 @@ def translate_text():
     return jsonify({'text': translate(request.form['text'],
                                       request.form['source_language'],
                                       request.form['dest_language'])})
+
+@bp.route('/search')
+@login_required
+def search():
+    if not g.search_form.validate():
+        return redirect(url_for('main.explore'))
+    page = request.args.get('page', 1, type=int)
+    posts, total = Post.search(g.search_form.q.data, page,
+                               current_app.config['POSTS_PER_PAGE'])
+    next_url = url_for('main.search', q=g.search_form.q.data, page=page + 1) \
+        if total > page * current_app.config['POSTS_PER_PAGE'] else None
+    prev_url = url_for('main.search', q=g.search_form.q.data, page=page - 1) \
+        if page > 1 else None
+    return render_template('search.html', title=_('Search'), posts=posts,
+                           next_url=next_url, prev_url=prev_url)
