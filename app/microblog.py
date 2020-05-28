2af29aae5c976a4033adf21da60f310bf8163bb7
ISAAC NYAKOI <isaacnyakoi1@gmail.com>
2020-05-28 06:35:19 +0300

    Second Commit

--- app/microblog.py
+++ app/microblog.py
@@ -1,10 +1,10 @@
-from app import create_app,db,cli
-from app.models import User,Post, Notification, Message
+# from app import create_app,db,cli
+# from app.models import User,Post, Notification, Message
 
-app = create_app()
-cli.register(app)
+# app = create_app()
+# cli.register(app)
 
-@app.shell_context_processor
-def make_shell_context():
-    return {'db': db, 'User': User, 'Post': Post, 'Message': Message,
-            'Notification': Notification}
+# @app.shell_context_processor
+# def make_shell_context():
+#     return {'db': db, 'User': User, 'Post': Post, 'Message': Message,
+#             'Notification': Notification}
