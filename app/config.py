55ccec555a87ca1cd647d432f3b924576a7c256d
ISAAC NYAKOI <isaacnyakoi1@gmail.com>
2020-05-28 06:08:13 +0300

    Second Commit

--- app/config.py
+++ app/config.py
@@ -1,21 +1,21 @@
-# import os
-# from dotenv import load_dotenv
-# basedir = os.path.abspath(os.path.dirname(__file__))
-# load_dotenv(os.path.join(basedir, '.env'))
+import os
+from dotenv import load_dotenv
+basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
 
-# class Config(object):
-#     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-#     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-#     	'sqlite:///' + os.path.join(basedir, 'app.db')
-#     SQLALCHEMY_TRACK_MODIFICATIONS = False
-#     MAIL_SERVER = os.environ.get('MAIL_SERVER')
-#     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
-#     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
-#     MAIL_USERNAME = os.environ.get('ISAAC_NYAKOI')
-#     MAIL_PASSWORD = os.environ.get('LION@JUDAH')
-#     ADMINS = ['riatkenya@gmail.com']
-#     POSTS_PER_PAGE = 25
-#     LANGUAGES = ['en', 'es']
-#     MS_TRANSLATOR_KEY = os.environ.get('MS_TRANSLATOR_KEY')
-#     ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL')
-#     LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+    	'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('ISAAC_NYAKOI')
+    MAIL_PASSWORD = os.environ.get('LION@JUDAH')
+    ADMINS = ['riatkenya@gmail.com']
+    POSTS_PER_PAGE = 25
+    LANGUAGES = ['en', 'es']
+    MS_TRANSLATOR_KEY = os.environ.get('MS_TRANSLATOR_KEY')
+    ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL')
+    LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
