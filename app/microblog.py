4a5241f5d8dc7426c55ebb55e0e1d8e231436893
ISAAC NYAKOI <isaacnyakoi1@gmail.com>
2020-05-28 09:19:51 +0300

    Second Commit

--- app/microblog.py
+++ app/microblog.py
@@ -1,1 +1,10 @@
+# from app import create_app,db,cli
+# from app.models import User,Post, Notification, Message
 
+# app = create_app()
+# cli.register(app)
+
+# @app.shell_context_processor
+# def make_shell_context():
+#     return {'db': db, 'User': User, 'Post': Post, 'Message': Message,
+#             'Notification': Notification}
